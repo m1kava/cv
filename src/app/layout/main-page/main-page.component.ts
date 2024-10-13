cfa65c5987c2a097165a327d861ec948f3bbd393
vmikava <vmikava@crocobet.com>
2024-10-14 00:05:16 +0400

    added more pages and styles

--- src/app/layout/main-page/main-page.component.ts
+++ src/app/layout/main-page/main-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {ProfileCardComponent} from '../profile-card/profile-card.component';
+import {ProfileTimelineComponent} from '../profile-timeline/profile-timeline.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
-    ProfileCardComponent
+    ProfileCardComponent,
+    ProfileTimelineComponent
   ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
