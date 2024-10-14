b2e82dec6c990be19298329f99f007b52cdbd756
vmikava <vmikava@crocobet.com>
2024-10-14 12:09:56 +0400

    responsive

--- src/app/layout/main-page/main-page.component.ts
+++ src/app/layout/main-page/main-page.component.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
-import {ProfileCardComponent} from '../profile-card/profile-card.component';
-import {ProfileTimelineComponent} from '../profile-timeline/profile-timeline.component';
+import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
+import { ProfileCardComponent } from '../profile-card/profile-card.component';
+import { ProfileTimelineComponent } from '../profile-timeline/profile-timeline.component';
+import {NgIf} from '@angular/common';
+
+const activeTab = {
+  Profile: 'Profile',
+  Timeline: 'Timeline'
+};
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
     ProfileCardComponent,
-    ProfileTimelineComponent
+    ProfileTimelineComponent,
+    NgIf
   ],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrls: ['./main-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainPageComponent {
+  activeTab = activeTab.Profile;
+  isSmallScreen = false;
 
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  // Method to set the active tab
+  setActiveTab(tab: string): void {
+    this.activeTab = tab;
+  }
+
+  // Listen to window resize events and update screen size
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkScreenSize();
+  }
+
+  // Check if the screen size is less than or equal to 768px
+  checkScreenSize(): void {
+    this.isSmallScreen = window.innerWidth <= 768;
+  }
 }
--- src/app/layout/profile-timeline/profile-timeline.component.ts
+++ src/app/layout/profile-timeline/profile-timeline.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-profile-timeline',
   standalone: true,
   imports: [],
   templateUrl: './profile-timeline.component.html',
-  styleUrl: './profile-timeline.component.scss'
+  styleUrl: './profile-timeline.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileTimelineComponent {
 
